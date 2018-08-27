0885f798bb7f666c3536d3534483641ea60c52a9
Bendazzoli <paulo.bendazzoli@matera.com>
2018-08-27 15:52:58 -0300

    Adicionando aulas bytebank herdado e encapsulado

---
+++ bytebank/src/bytebank_herdado/EditorVideo.java
@@ -0,0 +1,10 @@
+package bytebank_herdado;
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de video");
+		return 150;
+	}	
+
+}
