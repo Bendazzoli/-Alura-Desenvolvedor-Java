126f3fe5ddd0175530b467434bc334a310490c84
Bendazzoli <paulo.bendazzoli@matera.com>
2018-08-27 16:25:45 -0300

    Criando contas corrente e poupanca

---
+++ bytebank/src/bytebank_herdado_conta/TesteContas.java
@@ -0,0 +1,18 @@
+package bytebank_herdado_conta;
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+	
+		ContaCorrente cc = new ContaCorrente(1, 123456);
+		cc.deposita(100);
+		
+		ContaPoupanca cp = new ContaPoupanca(1, 654321);
+		cp.deposita(200);
+		
+		cc.transfere(10, cp);
+		
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
+	}
+}
